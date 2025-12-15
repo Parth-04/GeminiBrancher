f2b836ff29e6be78a21cce7d8bd7747524882833
Parth Rasal <parth.rasal@yahoo.com>
2025-12-15 22:47:43 +0200

    Add API talking functionality

--- sidepanel.js
+++ sidepanel.js
@@ -1,6 +1,11 @@
+const GEMINI_API_KEY = "AIzaSyA8sLCX-hlT6ci6sad_4NpuhOAvM3iB90k";
+
+let currentContext = "";
+
 chrome.storage.local.get(['geminiChatContext'], (result) => {
     if (result.geminiChatContext){
-        updateUI(result.geminiChatContext);
+        currentContext = result.geminiChatContext;
+        updateUI(result.geminiChatContext, "storage");
     }
 })
 
@@ -18,9 +23,9 @@ document.getElementById("scrapeBtn").addEventListener('click', async () => {
         }
 
         if (response.status === "success") {
+            currentContext = response.context;
             chrome.storage.local.set({'geminiChatContext': response.context});
-            updateUI(response.context);
-            // outputBox.innerText = "Success! Data Found: \n" + response.context;
+            updateUI(currentContext, "updated");
         } else if (response.status === "error") { 
             outputBox.innerText = "Error \n" + response.message;
             console.log(response.message);
@@ -33,14 +38,100 @@ document.getElementById("scrapeBtn").addEventListener('click', async () => {
     }
 });
 
-function updateUI(text) {
+document.getElementById("askBtn").addEventListener('click', async() => {
+    console.log("Submit button clicked!");
+    
+    const question = document.getElementById('user-input').value;
+    const responseBox = document.getElementById('ai-response');
+
+    console.log("Question: ", question);
+
+
+    if (!currentContext) {
+        responseBox.innerText = "Please Update the Context First!";
+        return;
+    }
+    if (!question){
+        responseBox.innerText = "Please ask a question";
+        return;
+    }
+
+    responseBox.innerText = "Thinking...";
+
+    try {
+        const prompt =  `
+        I have a conversation transcript between the user and the AI below, please answer the user's query based on it:
+
+        --- TRANSCRIPT START ---
+        ${currentContext}
+        --- TRANSCRIPT START ---
+
+        User Question: ${question}
+        Important Guideline: Make sure to answer the question in a maximum of 5 lines.
+        `;
+
+        const answer = await callGeminiAPI(prompt);
+        
+        if (typeof marked !== 'undefined'){
+            responseBox.innerHTML = marked.parse(answer);
+        } else{
+            responseBox.innerText = answer;
+        }
+
+
+    } catch (error) {
+        responseBox.innerText = "API Error: " + error.message;
+    }
+});
+
+function updateUI(text, status) {
     const outputBox = document.getElementById('debug-output');
 
     if (typeof marked !== 'undefined') {
-        outputBox.innerHTML = marked.parse(text);
+        if (status === "storage"){
+            outputBox.innerHTML = "Fetched from Storage \n" + marked.parse(text);
+        }
+        else if (status === "updated"){
+            outputBox.innerHTML = "Context Freshly Updated \n" + marked.parse(text);
+        } else {
+            outputBox.innerHTML = "Status Unknown \n" + marked.parse(text);
+        }
+        
     } else {
         outputBox.innerText = text;
     }
 
     outputBox.scrollTop = outputBox.scrollHeight;
+}
+
+async function callGeminiAPI(prompt) {
+    const model = "gemini-2.5-flash";
+
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${GEMINI_API_KEY}`;
+
+    const payload = {
+        contents: [{
+            parts: [{
+                text: prompt
+            }]
+        }],
+        generationConfig: {
+            temperature: 0.4,
+            topK: 40,
+            topP: 0.95
+        }
+    };
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify(payload)
+    });
+
+    if(!response.ok){
+        throw new Error(`Server Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.candidates[0].content.parts[0].text;
 }
